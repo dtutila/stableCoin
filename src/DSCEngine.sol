8f358ff89babbee5b25421f859bc5d0bc43f3c98
dtutila <daniel@dtutila.com>
2024-05-01 15:19:13 -0600

    initial DSCEngine contract

---
+++ src/DSCEngine.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.25;
+
+/// @title DSCEngine 
+contract DSCEngine {
+
+  function depositCollatalANdMintDSC() external {}
+  function redeemColateralForDSC() external {}
+  function redeemColateral() external {}
+  function mintDSC() external {}
+  function burnDSC() external {}
+  function liquidate() external {}
+  function healthFactor() external view {}
+
+ 
+}
