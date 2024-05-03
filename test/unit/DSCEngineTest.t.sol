c1aa882d7802a47dafe0b15820cca407fe178f26
dtutila <daniel@dtutila.com>
2024-05-03 17:46:37 -0600

    added test methods

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -6,6 +6,7 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "../../test/mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC private deployer;
@@ -15,17 +16,28 @@ contract DSCEngineTest is Test {
     address ethUSDpriceFeed;
     address weth;
 
+
+    //users
+    address alice = makeAddr('Alice');
+    uint256 private constant INTITIAL_ETH_BALANCE = 20 ether;
+    uint256 private constant COLLATERAL_AMOUNT = 10 ether;
+    uint256 private constant INITIAL_TOKEN_BALANCE = 100 ether;
+    
+
     function setUp() public {
       deployer = new DeployDSC();
 
       (dsc, engine, config) = deployer.run();
       (ethUSDpriceFeed,, weth,,) = config.activeNetworkConfig(); 
+      vm.deal(alice, INTITIAL_ETH_BALANCE);
+
+      ERC20Mock(weth).mint(alice, INITIAL_TOKEN_BALANCE);
 
     }
 
     // price Test
 
-    function testGestUSDValue() public {
+    function testGestUSDValue() public view {
       uint256 ethAmount = 15e18;
       // 15e18 * 2000/ETH = 30000e18
       uint256 expectedUSD = 30000e18;
@@ -33,6 +45,16 @@ contract DSCEngineTest is Test {
       uint256 actualUSD = engine.getUsdValue(weth, ethAmount);
 
       assertEq(expectedUSD, actualUSD);
-
     }
+  
+    function testRevertsIfCollateralZero() public {
+      vm.prank(alice);
+      ERC20Mock(weth).approve(address(engine), COLLATERAL_AMOUNT);
+     
+      vm.expectRevert(DSCEngine.DSCEngine__AmountMustBeGreaterThanZero.selector);
+      engine.depositCollateral(weth,0);
+
+      vm.stopPrank();
+    }
+
 }
