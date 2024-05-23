bbb6464965971646e2b0b4dc565ed67ebdb89c99
dtutila <daniel@dtutila.com>
2024-05-22 19:46:17 -0600

    updated contract version

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.25;
+pragma solidity 0.8.25;
 
 import {Script, console} from "forge-std/Script.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.25;
+pragma solidity 0.8.25;
 
 import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
